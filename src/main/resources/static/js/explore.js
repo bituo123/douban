c536192bc0b0d70d989fcccc242572fa2a411503
bituo123 <60815483+bituo123@users.noreply.github.com>
2022-10-01 21:31:43 +0800

    Update explore.js

--- src/main/resources/static/js/explore.js
+++ src/main/resources/static/js/explore.js
@@ -1,3 +1,4 @@
+//加载更多，给每一个加上样式类
 const changeIcon = document.querySelector('.loadmore');
 changeIcon.addEventListener('click', function() {
   const x = document.querySelectorAll('.channel-img .item');
